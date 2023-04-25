a4a7837f7f19ae9c411c281d76212d7f37b86cac
Orkhan647 <okuw.k@mail.ru>
2023-04-25 20:58:03 +0300

    Js Task 7

---
+++ js task 7/app.js
@@ -0,0 +1,67 @@
+let Musiqi = prompt(`Zehmet Olmasa Sevdiyin musiqini daxil et
+1. Джиган - На край Света
+2. Kaoma - Lambada
+3. Джиган -ДНК
+4. Song by Coolio and Kylian Mash - Gangsta's Paradise
+
+
+`)
+if(Musiqi === `1`){
+console.log(`Я часто вспоминаю наш последний вечер -
+Два билета в кино, ты опоздала на встречу.
+В легком платье, не прикрывающем плечи,
+И эту улыбку даже крыть нечем.
+
+И я почувствовал себя героем фильма
+"Освобожденный Джанго", чертов Тарантино.
+Мы по сценарию с тобой боролись за свободу,
+Но только друг от друга освободив по ходу.
+`)
+}
+else if (Musiqi === `2`){
+    console.log(`Chorando se foi quem um dia só me fez chorar
+    Chorando se foi quem um dia só me fez chorar
+    Chorando estará ao lembrar de um amor
+    Que um dia não soube cuidar
+    Chorando estará ao lembrar de um amor
+    Que um dia não soube cuidar
+    A recordação vai estar com ele aonde for
+    A recordação vai estar pra sempre aonde for
+    Dança sol e mar, guardarei no olhar
+    O amor faz perder e encontrar`)
+}
+else if ( Musiqi === `3`){
+    console.log(`Они хотят со мной быть,
+    Но я только твой, любовь без остатка.
+    Мы сходим с ума, у всех на глазах,
+    И нам все не важно.
+    Опять карусель, по телу вновь дрожь.
+    Ещё один день, ещё одна ночь.
+    Ещё один город и снова в тебе.
+    Всё будто во сне, ты снова на мне, е!
+    И мы набираем с ней скорость.
+    Пустой хайвей, ночь вокруг, невесомость.
+    Я её крепость, но это не новость.
+    Она за мной, даже если я в пропасть.
+    Вместе мы в сто сильней чем порознь.
+    Греет, когда я в дали её голос.
+    Вместе мы высоко, под нами облака.
+    Она в моей ДНК.`)
+}
+else if (Musiqi === `4`){
+    console.log(`As I walk through the valley of the shadow of death
+    I take a look at my life and realize there's nothin' left
+    'Cause I've been blastin' and laughin' so long, that
+    Even my momma thinks that my mind is gone
+    But I ain't never crossed a man that didn't deserve it
+    Me be treated like a punk, you know that's unheard of
+    You better watch how you talkin' and where you walkin'
+    Or you and your homies might be lined in chalk
+    I really hate to trip, but I gotta loc
+    As they croak, I see myself in the pistol smoke, fool
+    I'm the kinda G the little homies wanna be like
+    On my knees in the night, sayin' prayers in the streetlight`)
+}
+else{
+    console.log(`Ups! Bele bir musiqi yoxdur ;)`)
+}
