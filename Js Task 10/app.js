922b5ec8f5c2662014e3b381cd001870767f98b4
Orkhan647 <okuw.k@mail.ru>
2023-04-27 12:59:38 +0300

    Js Task 10

---
+++ Js Task 10/app.js
@@ -0,0 +1,6 @@
+let selsi = +prompt("zehmet olmasa selsini yazin");
+let fahrenhit;
+
+if ((fahrenhit = selsi * 1.8 + 32)) {
+  console.log("Fahrenhit: " + fahrenhit + " °F");
+} else console.log("zehmet olmasa selsini duzdun yaz ...");
