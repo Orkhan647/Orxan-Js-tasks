efb355cb2fdad46836b689309ca3a6d36747c1ac
Orkhan647 <okuw.k@mail.ru>
2023-04-27 12:12:35 +0300

    Js Task 9

---
+++ Js Task 9/app.js
@@ -0,0 +1,9 @@
+let ed = +prompt(`Zehmet olmasa eded daxil edin`);
+if (ed > 0) {
+  console.log("Eded müsbətdir.");
+} else if (ed < 0) {
+  console.log("Eded mənfidir.");
+} else if ((ed === 0)) {
+  console.log("Eded sifirdir.");
+}
+else{console.log("Bu eded Deyil")}
