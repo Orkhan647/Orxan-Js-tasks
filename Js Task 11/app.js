e884612e89e24e3a5ce455c015c7773654bf51c3
Orkhan647 <okuw.k@mail.ru>
2023-04-27 13:05:43 +0300

    Js Task 11

---
+++ Js Task 11/app.js
@@ -0,0 +1,9 @@
+let eded = +prompt("zehmet olmasa eded daxil edin")
+if(eded % 2 === 0) {
+  console.log(" eded cutdur.");
+} else if (eded % 2 === 1){
+    console.log("eded tekdir.")
+}
+else{
+    console.log("bu eded deyil")
+}
