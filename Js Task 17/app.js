0677c5fb666334375d8d1fa16fc00a15922d9e9e
Orkhan647 <okuw.k@mail.ru>
2023-04-27 14:38:31 +0300

    Js Task 17

---
+++ Js Task 17/app.js
@@ -0,0 +1,7 @@
+let eded = +prompt("Zəhmət olmasa bir edəd daxil edin:");
+
+if (eded % 2 === 0 && eded % 5 === 0) {
+  console.log("Bu istədiyim edəddir");
+} else {
+  console.log("istediyim deyil");
+}
