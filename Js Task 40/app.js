4a56f6bfbeb4f4f0c22e3ce38dc43d5cf9b19cc8
Orkhan647 <okuw.k@mail.ru>
2023-05-06 22:45:44 +0300

    Js Task 40

---
+++ Js Task 40/app.js
@@ -0,0 +1,6 @@
+const firstName = prompt('Zehmet olmasa adinizi daxil edin')
+const lastName = prompt('Zehmet olmasa soy adinizi daxil edin')
+const msg = ()=>{
+    return `“Sizin adınız ${firstName} ${lastName} Tanışlığımıza çox şadam!”`
+}
+console.log(msg());
