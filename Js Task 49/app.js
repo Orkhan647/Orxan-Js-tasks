54031e7a77431c073a9630e0b56057641176601c
Orkhan647 <okuw.k@mail.ru>
2023-05-07 23:02:45 +0300

    Js Task 49

---
+++ Js Task 49/app.js
@@ -0,0 +1,7 @@
+let num = prompt('zehmet olmasa eded daxil edin');
+
+let quvvet = num ** 5;
+console.log("Ədədin beşinci dərəcə qüvvətə qaldirilmis vəziyyəti:", quvvet);
+
+let qaliq = num % 3;
+console.log("3-ə bölünmədən qalan qaliq:", qaliq);
