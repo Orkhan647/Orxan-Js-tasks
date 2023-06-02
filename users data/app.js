e5ddacc24140b3e9f4f0efd8746188874ffc0a14
Orkhan647 <okuw.k@mail.ru>
2023-06-02 15:05:35 +0300

    Finish Task #Users Data

--- users data/app.js
+++ users data/app.js
@@ -50,3 +50,29 @@ const users=[
 ]
 
 
+const topCardContainer = document.getElementById("top-cards");
+const bottomCardContainer = document.getElementById("bottom-cards");
+
+users.forEach((user, index) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const image = document.createElement("img");
+  image.src = `img/${user.img}.jpg`; // Assuming the images have jpg extension and are named as "ali.jpg", "bedbext_developer.jpg", etc.
+  image.classList.add("card-image"); // Add a class to the image element
+  card.appendChild(image);
+
+  const username = document.createElement("h3");
+  username.textContent = user.username;
+  card.appendChild(username);
+
+  const job = document.createElement("p");
+  job.textContent = user.job;
+  card.appendChild(job);
+
+  if (index < users.length / 2) {
+    topCardContainer.appendChild(card);
+  } else {
+    bottomCardContainer.appendChild(card);
+  }
+});
