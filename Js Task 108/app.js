326061931d87efd3ef1766cb658c79b114226f24
Orkhan647 <okuw.k@mail.ru>
2023-06-08 18:41:12 +0300

    Js Task 108

---
+++ Js Task 108/app.js
@@ -0,0 +1,14 @@
+const users = [
+  "Orxan",
+  "Kerimov",
+  "Elman",
+  "React",
+  "Vs Code",
+  "Node.js",
+  "Jed Academy",
+  "Javascript",
+  "Html5",
+  "Css",
+];
+const [person1,person2,person3,...others]=users
+console.log(person1,person2,person3,others);
