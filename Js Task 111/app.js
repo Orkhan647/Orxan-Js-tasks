d076f1b23dfe4ad2dd713e514d0a4606580fd93c
Orkhan647 <okuw.k@mail.ru>
2023-06-10 23:39:32 +0300

    repair task

--- Js Task 111/app.js
+++ Js Task 111/app.js
@@ -1,6 +1,11 @@
-function toCapitalCase(str) {
-  return str.replace(str[0], str[0].toUpperCase());
+function CustomString(str){
+  this.str = str;
 }
 
-const ad = "orxan";
-console.log(toCapitalCase(ad));
+CustomString.prototype.toCapitalCase= function() {
+  return this.str.replace(this.str[0], this.str[0].toUpperCase());
+}
+
+
+let myString = new CustomString('orxan')
+console.log(myString.toCapitalCase());
