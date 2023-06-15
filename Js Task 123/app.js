2ec4ee918d2512dd48f0759fbb00c62d52a5fe6e
Orkhan647 <okuw.k@mail.ru>
2023-06-15 22:25:31 +0300

    update

--- Js Task 123/app.js
+++ Js Task 123/app.js
@@ -1,19 +1,17 @@
-const url = "https://api.github.com/users/Ali-GreenHeart/followers"
-const rootElem= document.getElementById('root')
+const url = "https://api.github.com/users/Ali-GreenHeart/followers";
+const rootElem = document.getElementById("root");
 
 fetch(url)
-.then((response)=>{
-    return response.json()
-})
-.then((data)=>{
-    data.forEach((followerData)=>{
-        const followeElem=document.createElement("div");
-        const  nameElem=document.createElement('p');
-        const avatarUrl=document.createElement('img');
-        avatarUrl.src=followerData.avatar_url
-        nameElem.innerHTML=`Name: ${followerData.login}`
-        rootElem.appendChild(avatarUrl)
-        rootElem.appendChild(nameElem)
-
-    })
-})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((followerData) => {
+      const nameElem = document.createElement("p");
+      const avatarUrl = document.createElement("img");
+      avatarUrl.src = followerData.avatar_url;
+      nameElem.innerHTML = `Name: ${followerData.login}`;
+      rootElem.appendChild(avatarUrl);
+      rootElem.appendChild(nameElem);
+    });
+  });
